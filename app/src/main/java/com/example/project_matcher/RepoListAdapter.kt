326376ab88db89cb748c4da8637846af1eb74775
celinarabe <celinarabe@gmail.com>
2022-03-17 11:25:40 -0500

    Glide image loading

--- app/src/main/java/com/example/project_matcher/RepoListAdapter.kt
+++ app/src/main/java/com/example/project_matcher/RepoListAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.project_matcher
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.rocketreserver.RepoListQuery
 
 class RepoListAdapter(private val context: Context, private val dataset : List<RepoListQuery.Edge?>?
@@ -14,6 +17,7 @@ class RepoListAdapter(private val context: Context, private val dataset : List<R
     class RepoViewHolder(private val view: View):RecyclerView.ViewHolder(view) {
         val title: TextView = view.findViewById(R.id.tvTitle)
         val description: TextView = view.findViewById(R.id.tvDescription)
+        val image : ImageView = view.findViewById(R.id.imgRepo)
 
     }
 
@@ -29,7 +33,12 @@ class RepoListAdapter(private val context: Context, private val dataset : List<R
         val item = dataset?.get(position)
         holder.title.text = item?.node?.nameWithOwner
         holder.description.text = item?.node?.description
+        val imageUrl = item?.node?.openGraphImageUrl
+        imageUrl?.let {
+            Glide.with(context).load(imageUrl).into(holder.image)
+        }
     }
+
 
     override fun getItemCount() = dataset?.size ?: 0
 }
